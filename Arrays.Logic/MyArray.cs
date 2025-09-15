0e7ea0a6285f3187a0304bba1f679d9b84f670d7
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-09-14 20:50:46 -0500

    Update Arrays

---
+++ Arrays.Logic/MyArray.cs
@@ -0,0 +1,213 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Arrays.Logic;
+
+public class MyArray
+{
+    private int _top;           //Logic size
+    private int[] _array;
+
+    public MyArray(int n)
+    {
+        N = n;
+        _array = new int[n];
+        _top = 0;
+    }
+
+    public bool IsFull => _top == N;
+
+    public bool IsEmpty => _top == 0;
+
+    public int N { get; }           //PROPIEDAD DE LECTURA - SOLO GET  -- Phisical size 
+
+    public void Add(int number)
+    {
+        if (IsFull)
+        {
+            throw new Exception("The array is full.");
+        }
+
+        _array[_top] = number;
+        _top++;
+    }
+
+    public void Insert(int number, int position) 
+    {
+        if (IsFull)
+        {
+            throw new Exception("The array is full.");
+        }
+        if (position < 0 || position > _top)
+        {
+            position = 0;
+        }
+        if (position > _top)
+        {
+            position = _top;
+        }
+
+        for (int i = _top; i > position; i--)
+        {
+            _array[i] = _array[i - 1];
+        }
+        _array[position] = number;
+        _top++;
+    }
+
+    public void Remove(int position)
+    {
+        if (IsEmpty)
+        {
+            throw new Exception("The array is empty.");
+        }
+        if (position < 0)
+        {
+            position = 0;
+        }
+        if (position > _top)
+        {
+            position = _top;
+        }
+
+        for (int i = position; i < _top - 1; i++)
+        {
+            _array[i] = _array[i + 1];
+        }
+        _top--;
+    }
+
+    public void Fill()
+    {
+        Fill(1, 100);
+    }
+
+    public void Fill(int minimum, int maximum)
+    {
+        Random random = new();
+        for (int i = 0; i < N; i++)
+        {
+            _array[i] = random.Next(minimum, maximum);
+            _top++;
+        }
+        _top = N;
+    }
+
+    public MyArray GetPrimes()
+    {
+        int primesCount = 0;
+        for (int i = 0; i < _top; i++)
+        {
+            if (IsPrime(_array[i]))
+            {
+                primesCount++;
+            }
+        }
+        var primesArray = new MyArray(primesCount);
+        for (int i = 0; i < _top; i++)
+        {
+            if (IsPrime(_array[i]))
+            {
+                primesArray.Add(_array[i]);
+            }
+        }
+        return primesArray;
+    }
+
+    public MyArray GetEvens()
+    {
+        var evensCount = 0;
+        for (int i = 0; i < _top; i++)
+        {
+            if (IsEven(_array[i]))
+            {
+                evensCount++;
+            }
+        }
+
+        var evensArray = new MyArray(evensCount);
+        for (int i = 0; i < _top; i++)
+        {
+            if (IsEven(_array[i]))
+            {
+                evensArray.Add(_array[i]);
+            }
+        }
+        return evensArray;
+
+    }
+
+    private bool IsEven(int n)
+    {
+        return n % 2 == 0;
+    }
+
+    public override string ToString()
+    {
+        if (IsEmpty)
+        {
+            return "Empty Array";
+        }
+
+        string output = string.Empty;
+        var count = 0;
+        for (int i = 0; i < _top; i++)
+        {
+            output += $"{_array[i]}\t";
+            count++;
+            if (count > 9)
+            {
+                count = 0;
+                output += "\n";
+            }
+        }
+        return output;
+    }
+
+    public void Sort()
+    {
+        Sort(descending: false);
+    }
+
+    public void Sort(bool descending)
+    {
+        for (int i = 0; i < _top - 1; i++)
+        {
+            for (int j = i + 1; j < _top; j++)
+            {
+                if (descending)
+                {
+                    if (_array[i] < _array[j])
+                    {
+                        Change(ref _array[i], ref _array[j]);
+                    }
+                }
+                else
+                {
+                    if (_array[i] > _array[j])
+                    {
+                        Change(ref _array[i], ref _array[j]);
+                    }
+                }
+            }
+        }
+    }
+
+    private bool IsPrime(int n)
+    {
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }    
+
+    private void Change(ref int a, ref int b)
+    {
+        int aux = a;
+        a = b;
+        b = aux;
+    }
+}
---
+++ Arrays.UI.Console/Program.cs
@@ -0,0 +1,84 @@
+﻿using Arrays.Logic;
+
+
+MyArray array = new(50000);
+array.Fill(1, 999999);
+Console.WriteLine(array);
+Console.WriteLine("=====================================");
+var date1 = DateTime.Now;
+var primes = array.GetPrimes();
+var date2 = DateTime.Now;
+var time = date2 - date1;
+primes.Sort();
+Console.WriteLine(primes);
+Console.WriteLine($"Time elapsed: {time.Minutes} minutes, {time.Seconds} seconds, {time.Milliseconds} milliseconds, {time.Nanoseconds} nanoseconds.");
+
+
+
+
+//:::::::::::::::::: Array Sort Evens :::::::::::::::::::
+//try
+//{
+//    MyArray array = new(500);
+//    array.Fill();
+//    Console.WriteLine(array);
+//    Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n");
+//    var evens = array.GetEvens();
+//    evens.Sort();
+//    Console.WriteLine(evens);
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+
+//::::::::::::: Insert and remove methods ::::::::::::::::
+//try
+//{
+//    MyArray array = new(5);
+//    array.Add(10);
+//    array.Add(20);
+//    array.Add(30);
+//    array.Insert(8, 0);              //number, position
+//    Console.WriteLine(array);
+//    array.Remove(0);              //number, position
+//    Console.WriteLine("___________________");
+//    Console.WriteLine(array);
+//}
+//catch (Exception ex)
+//{
+//   Console.WriteLine(ex.Message);
+//}
+
+//::::::::::: Add method::::::::::::::
+//try
+//{
+//       MyArray array = new(5);
+//        array.Add(10);
+//      array.Add(20);
+//        array.Add(30);
+//        array.Add(40);
+//        array.Add(50);
+//        array.Add(60);
+//        Console.WriteLine(array);
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+    
+
+//:::::::::::: Sort array :::::::::::::::
+//MyArray array = new(5);
+//array.Fill(1, 100);
+//Console.WriteLine("Unsorted array");
+//Console.WriteLine(array);
+
+//array.Sort(descending: false);
+//Console.WriteLine("Ascending Sorted array");
+//Console.WriteLine(array); 
+
+//array.Sort(descending: true);  
+//Console.WriteLine("Descending Sorted array");
+//Console.WriteLine(array); 
