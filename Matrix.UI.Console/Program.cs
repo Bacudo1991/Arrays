922fd0caa1abd3aad2a620b031797e2e83b6e035
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-09-17 14:54:25 -0500

    Update Arrays

---
+++ Matrix.UI.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using Arrays.Logic;
+
+try
+{
+    var myMatrix = new MyMatrix();
+    myMatrix.Fill();
+    var mainDiagonal = myMatrix.GetMainDiagonal();
+    var secondaryDiagonal = myMatrix.GetSecondaryDiagonal();
+
+    Console.WriteLine("\n:::::::::: MATRIX ::::::::::\n");
+    Console.WriteLine(myMatrix);
+    Console.WriteLine("\n::::::: MAIN DIAGONAL ::::::\n");
+    Console.WriteLine(mainDiagonal);
+    Console.WriteLine("\n::::: SECONDARY DIAGONAL :::\n");
+    Console.WriteLine(secondaryDiagonal);
+    Console.WriteLine("\n::::: UPPER TRIANGULAR :::::\n");
+    Console.WriteLine(myMatrix.GetUpperTriangular());
+    Console.WriteLine("\n::::: LOWER TRIANGULAR :::::\n");
+    Console.WriteLine(myMatrix.GetLowerTriangular());
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
